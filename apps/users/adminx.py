2c4cbb12ecce86be7d9ca9a9424936c3e45e1e38
席栋祥 <1456121347@qq.com>
2020-05-28 10:45:30 +0800

    "支付宝支付（公钥，私钥，支付宝公钥）"

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -8,7 +8,7 @@ class BaseSetting(object):
 
 class GlobalSettings(object):
     site_title = "海马生鲜后台管理系统"
-    site_footer = "北京网络职业技术学院 - 2020"
+    site_footer = "海马生鲜管理系统 - 2020"
     menu_style = "accordion"
 
 
